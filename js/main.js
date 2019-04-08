7ece01a4a10223faeec20754cad26f9e63699efe
何娟娟； <3406329515@qq.com>
2019-04-08 19:28:26 +0800

    only over

--- js/main.js
+++ js/main.js
@@ -6,7 +6,11 @@ $(function(){
     $("html,body").animate({scrollTop: 0}, 600);
     });
     
-	
+	 $(".slide>p").click(function(){
+        if(scroll=="off") return;
+    $("html,body").animate({scrollTop: 0}, 600);
+    });
+    
 	
 	
 	
